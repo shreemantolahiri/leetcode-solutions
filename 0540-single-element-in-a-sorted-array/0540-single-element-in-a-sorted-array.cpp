4c7b7b3ca1a68cc097efbb31ccbb74e1dde4e76b
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-06-28 18:54:32 +0530

    Time: 17 ms (73.29%), Space: 24.7 MB (14.03%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,6 +7,8 @@ public:
         //trim down the search space so that NEVER out of bound
         int left=1;
         int right=n-2;
+
+        //base conditions for edge cases
         if(nums[0]!=nums[1]) return nums[0];
 
         if(nums[n-1]!=nums[n-2]) return nums[n-1];
