8617233096909ed80000038b6b1c65a27acacad4
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-06-28 22:09:50 +0530

    Time: 3 ms (37.63%), Space: 7.5 MB (39.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,12 +4,12 @@ public:
         int left=1; int right=x/2;
         if(x==0 || x==1) return x;
 
-        int ans=0;
+        // int ans=0;
         while(left<=right){
             long long mid=left+(right-left)/2;
 
             if(mid*mid<=x){
-                ans=mid;
+                // ans=mid;
                 left=mid+1;
             }
             else{
@@ -18,6 +18,6 @@ public:
             }
         }
 
-        return ans;
+        return right;
     }
 };
