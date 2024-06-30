53be637bace4517de06209bfd3e619f662e7d41b
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-06-30 22:26:06 +0530

    Time: 723 ms (5.04%), Space: 242.1 MB (6.68%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,28 +1,53 @@
 class Solution {
-    void dfs(int x, int curr, vector<vector<int>>& ans, vector<vector<int>>& directChild)
-    {
-        for(int ch : directChild[curr])
-        {
-            if(ans[ch].empty() || ans[ch].back() != x)
-            {
-                ans[ch].push_back(x);
-                dfs(x, ch, ans, directChild);
+public:
+    void dfs(int par,unordered_map<int,vector<int>> &adj,map<int,set<int>> &mp,int n){
+        vector<int> vis(n,0);
+        vis[par]=1;
+        queue<int> q;
+        q.push(par);
+        // cout<<par<<" parent"<<endl;
+        while(!q.empty()){
+            int v=q.front();
+            q.pop();
+
+            for(auto it:adj[v]){
+                if(!vis[it]){
+                    mp[it].insert(par);
+                    // cout<<it<<" has ancestor= "<<par<<endl;
+                    vis[it]=1;
+                    q.push(it);
+                }
             }
         }
-    }
 
-public:
+        return;
+    }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> ans(n), directChild(n);
-        for(auto& e : edges)
-        {
-            directChild[e[0]].push_back(e[1]);
+        map<int,set<int>> mp;
+        unordered_map<int,vector<int>> adj;
+
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            // cout<<edges[i][0]<<"  "<<edges[i][1]<<endl;
         }
-        for
-        (int i=0;i<n;i++)
-        {
-            dfs(i, i, ans, directChild);
+
+        //we perform dfs over every element
+        for(int i=0;i<n;i++){
+            dfs(i,adj,mp,n);
         }
-        return ans;
+
+        //final result
+        vector<vector<int>> res(n);
+        
+        int index = 0;
+
+        for (int i=0;i<n;i++) {
+        // Convert set<int> to vector<int>
+        if(mp.find(i)==mp.end()) continue;
+        res[i] = vector<int>(mp[i].begin(), mp[i].end());
+        }
+
+        return res;
+
     }
 };
