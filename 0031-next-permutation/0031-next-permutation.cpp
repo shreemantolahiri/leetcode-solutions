4a8d7eab281a4d0d62564f1ae0077e64a0adea52
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-07-08 21:13:01 +0530

    Time: 0 ms (100%), Space: 14.5 MB (87.17%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int i=n-2;
-        for(i=n-2;i>=0;i--){
-            if(nums[i+1]<=nums[i]) continue;
-            else break;
+        int n = nums.size(); // Get the size of the array
+        int i = n - 2; // Initialize i to the second last index
+
+        // Find the first pair of two successive numbers a[i] and a[i+1], from the end, which satisfy a[i] < a[i+1]
+        for (i = n - 2; i >= 0; i--) {
+            if (nums[i + 1] <= nums[i]) 
+                continue; // If nums[i+1] is not greater than nums[i], continue searching
+            else 
+                break; // If we find nums[i] < nums[i+1], break the loop
         }
-        if(i==-1){
-            sort(nums.begin(),nums.end());
-        }
-        else{
-            //we break at index i
-            cout<<nums[i]<<endl;
-            //find number larger than nums[i]
-            int j=i+1; int ind=i+1;
-            for(int k=i+2;k<n;k++){
-                if(nums[k]>nums[i] && nums[k]<nums[j]){
-                    j=k;
+
+        // If no such pair is found, it means the array is sorted in descending order
+        if (i == -1) {
+            // Reverse the entire array to get the smallest permutation
+            // O(N)
+            reverse(nums.begin(), nums.end());
+        } else {
+            // We break at index i
+            cout << nums[i] << endl; // Output the value at index i for debugging
+
+            // Find the smallest number greater than nums[i] to the right of nums[i]
+            int j = i + 1;
+            for (int k = i + 1; k < n; k++) {
+                if (nums[k] > nums[i] && nums[k] <= nums[j]) {
+                    j = k;
                 }
             }
 
-            swap(nums[i],nums[j]);
+            // Swap the number at i with this number
+            swap(nums[i], nums[j]);
 
-            sort(nums.begin()+i+1,nums.end());
+            // Reverse the numbers following i to get the next permutation
+            reverse(nums.begin() + i + 1, nums.end());
         }
     }
 };
