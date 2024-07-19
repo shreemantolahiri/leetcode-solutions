952d9bc10e4f4269c7057c222ae9281b9b709215
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-07-20 00:19:55 +0530

    Time: 654 ms (22.18%), Space: 142.8 MB (22.95%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void bfs(int node, vector<int>& dist, vector<int>& edges) {
+        unordered_set<int> vis;
+
+        queue<int> q;
+        q.push(node);
+        vis.insert(node);
+        int lvl = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+
+            for (int i = 0; i < sz; i++) {
+                int curr = q.front();
+                q.pop();
+                if(dist[curr]==-1) dist[curr]=lvl;
+
+                int nextNode=edges[curr];
+                if(nextNode!=-1 && vis.find(nextNode)==vis.end()){
+                    q.push(nextNode);
+                    vis.insert(nextNode);
+                }
+            }
+
+            lvl++;
+        }
+
+        return;
+    }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+
+        vector<int> dist1(n + 1, -1), dist2(n + 1, -1);
+        bfs(node1,dist1,edges);
+        bfs(node2,dist2,edges);
+
+        for(auto it:dist1) cout<<it<<" ";
+        cout<<endl;
+        for(auto it:dist2) cout<<it<<" ";
+        cout<<endl;
+        int minDist=INT_MAX; int node=-1;
+        for(int i=0;i<=n;i++){
+            if(dist1[i]!=-1 && dist2[i]!=-1){
+                if(minDist>max(dist1[i],dist2[i])){
+                    node=i;
+                    minDist=max(dist1[i],dist2[i]);
+                }
+            }
+        }
+        return node;
+        // start dfs from node1
+    }
+};
