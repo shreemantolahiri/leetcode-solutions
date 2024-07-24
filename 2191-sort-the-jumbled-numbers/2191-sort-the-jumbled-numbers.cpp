c5b2d8ca83b8887f85d6dd9a49c6bd41385fc976
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-07-24 12:41:42 +0530

    Time: 724 ms (15.15%), Space: 102 MB (34.2%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-    int mapped(int a,vector<int>& mapping){
-        string s=to_string(a);
-        int num=0;
-        for(int i=0;i<s.length();i++){
-            int curr=s[i]-'0';
-            int map=mapping[curr];
-            num=num*10+map;
+    // This function maps the digits of a number 'a' using the provided 'mapping' array
+    int mappedNumber(int a, vector<int>& mapping) {
+        // Convert the integer 'a' to a string to handle each digit individually
+        string s = to_string(a);
+        int num = 0; // This will store the newly mapped number
+        
+        // Loop through each character in the string representation of the number
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s[i] - '0';  // Convert the character to an integer digit
+            int map = mapping[curr];  // Get the mapped value for the current digit
+            num = num * 10 + map;  // Construct the new number using the mapped digits
+        }
+
+        return num;  // Return the mapped number
+    }
+
+    // This function sorts the 'nums' array based on the mapped values of the numbers
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]=mappedNumber(nums[i],mapping);
         }
         
-        // int mul=1;
-        // while(a>0){
-        //     int dig=a%10;
-        //     num+=mapping[dig]*mul;
-        //     a=a/10;
-        //     mul*=10;
-        // }
-
-        return num;
-    }
-    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        // Use the sort function with a custom comparator lambda function
         
-        sort(nums.begin(),nums.end(),[&](int a,int b){
-            int mappeda=mapped(a,mapping);
-            int mappedb=mapped(b,mapping);
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            // Get the mapped values for both numbers a and b
+            int mappeda = mp[a];
+            int mappedb = mp[b];
 
-            if(mappeda==mappedb){
-                return false;
-            }
-            else{
-                return mappeda<mappedb;
+            // If the mapped values are equal, maintain their original order
+            if (mappeda == mappedb) {
+                return false;  // Since they are equal, return false (no need to swap)
+            } else {
+                // Otherwise, sort based on the mapped values
+                return mappeda < mappedb;  // Sort in ascending order of mapped values
             }
         });
 
-        return nums;
+        return nums;  // Return the sorted array
     }
 };
