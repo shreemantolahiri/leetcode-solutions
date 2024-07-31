b16a106a81a18e24743fe5cfdda61f64118a7f60
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-08-01 00:38:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int solve(int ind,int row, int col,vector<vector<int>>& books, int shelfWidth){
+        if(ind==books.size()) return 0;
+
+        int currWidth=books[ind][0];
+        int currHeight=books[ind][1];
+    }
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        
+    }
+};
