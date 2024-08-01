0a280c733e7facd3f5e8a7a36fe9662348a0a66f
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-08-01 19:17:37 +0530

    Time: 11 ms (14.3%), Space: 17.5 MB (31.37%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        int n=details.size();
+        for(int i=0;i<n;i++){
+            char c1=details[i][11];
+            char c2=details[i][12];
+
+            int age=c2-'0';
+            age+=(c1-'0')*10;
+            cout<<age<<endl;
+            if(age>60) count++;
+        }
+
+        return count;
+    }
+};
