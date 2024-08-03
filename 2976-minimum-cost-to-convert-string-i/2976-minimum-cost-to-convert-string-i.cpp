9eec3107b70d0b108a712cb2c1b8b40f60d3dff6
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-08-03 18:33:06 +0530

    Time: 157 ms (48.96%), Space: 96.7 MB (51.97%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,63 +1,44 @@
 class Solution {
 public:
-
-    void dijkstra(vector<vector<long long>> &res, unordered_map<char,vector<pair<char,long long>>> &mp, char s){
-        priority_queue< pair<long long,char>, vector<pair<long long,char>>, greater<pair<long long,char>>> pq;
-        
-        
-            // char s=source[i];
-            // res[s-'a'][s-'a']=0;
-            pq.push({0,s});
-            while(!pq.empty()){
-                
-                long long d=pq.top().first;
-                char midNode=pq.top().second;
-                pq.pop();
-                for(auto &vec:mp[midNode]){
-                    char node=vec.first;
-                    long long wt=vec.second;
-
-                    if(d+wt<res[s-'a'][node-'a']){
-                        res[s-'a'][node-'a']=d+wt;
-                        pq.push({d+wt,node});
+    //prims algorithm
+    void floydWarshall(vector<vector<int>> &adj) {
+        int V = adj.size();
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (adj[i][k] != INT_MAX && adj[k][j] != INT_MAX) {
+                        adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
                     }
                 }
             }
-
-        
-
-        return;
+        }
     }
+    long long minimumCost(string source, 
+    string target, vector<char>& original, 
+    vector<char>& changed, 
+    vector<int>& cost) {
+        vector<vector<int>> adj(26,vector<int>(26,INT_MAX));
+        int n=original.size();
 
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        
-        vector<vector<long long>> res(26, vector<long long>(26,INT_MAX));
+        for(int i=0;i<n;i++){
+            char src=original[i];
+            char dst=changed[i];
+            int c=cost[i];
 
-        //make a adjacency list
-        unordered_map<char,vector<pair<char,long long>>> mp;
-
-        for(int i=0;i<original.size();i++){
-            mp[original[i]].push_back({changed[i],(long long)cost[i]});
-        }
-
-        //perform dijkstras algorithm
-        for(int i=0;i<source.size();i++){
-            dijkstra(res,mp,source[i]);
-        }
-        
-
-        long long mincost=0;
-        //now all shortest paths calculated 
-        for(int i=0;i<source.size();i++){
-
-                if(source[i]==target[i]) continue;
-
-                if(res[source[i]-'a'][target[i]-'a']==INT_MAX) return -1;
-                mincost+=res[source[i]-'a'][target[i]-'a'];
-                
+            //shortest path
+            adj[src-'a'][dst-'a']=min(c,adj[src-'a'][dst-'a']);
             
         }
-
-        return mincost;
+        floydWarshall(adj);
+        long long res=0;
+        for(int i=0;i<source.length();i++){
+            if(source[i]==target[i]) continue;
+            else{
+                //cannot be reached
+                if(adj[source[i]-'a'][target[i]-'a']==INT_MAX) return -1;
+                res+=adj[source[i]-'a'][target[i]-'a'];
+            }
+        }
+        return res;
     }
 };
