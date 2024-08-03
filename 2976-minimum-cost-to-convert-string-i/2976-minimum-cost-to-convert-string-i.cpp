bad1da2cffc2fb27cdf2fe603dcb663c93b5f338
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-08-03 18:39:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    //prims algorithm
-    void floydWarshall(vector<vector<int>> &adj) {
-        int V = adj.size();
-        for (int k = 0; k < V; k++) {
-            for (int i = 0; i < V; i++) {
-                for (int j = 0; j < V; j++) {
+    // prims algorithm
+    void floydWarshall(vector<vector<int>>& adj) {
+        int n = adj.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
                     if (adj[i][k] != INT_MAX && adj[k][j] != INT_MAX) {
                         adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
                     }
@@ -13,30 +14,29 @@ public:
             }
         }
     }
-    long long minimumCost(string source, 
-    string target, vector<char>& original, 
-    vector<char>& changed, 
-    vector<int>& cost) {
-        vector<vector<int>> adj(26,vector<int>(26,INT_MAX));
-        int n=original.size();
+    long long minimumCost(string source, string target, vector<char>& original,
+                          vector<char>& changed, vector<int>& cost) {
+        vector<vector<int>> adj(26, vector<int>(26, INT_MAX));
+        int n = original.size();
 
-        for(int i=0;i<n;i++){
-            char src=original[i];
-            char dst=changed[i];
-            int c=cost[i];
+        for (int i = 0; i < n; i++) {
+            char src = original[i];
+            char dst = changed[i];
+            int c = cost[i];
 
-            //shortest path
-            adj[src-'a'][dst-'a']=min(c,adj[src-'a'][dst-'a']);
-            
+            // shortest path
+            adj[src - 'a'][dst - 'a'] = min(c, adj[src - 'a'][dst - 'a']);
         }
         floydWarshall(adj);
-        long long res=0;
-        for(int i=0;i<source.length();i++){
-            if(source[i]==target[i]) continue;
-            else{
-                //cannot be reached
-                if(adj[source[i]-'a'][target[i]-'a']==INT_MAX) return -1;
-                res+=adj[source[i]-'a'][target[i]-'a'];
+        long long res = 0;
+        for (int i = 0; i < source.length(); i++) {
+            if (source[i] == target[i])
+                continue;
+            else {
+                // cannot be reached
+                if (adj[source[i] - 'a'][target[i] - 'a'] == INT_MAX)
+                    return -1;
+                res += adj[source[i] - 'a'][target[i] - 'a'];
             }
         }
         return res;
