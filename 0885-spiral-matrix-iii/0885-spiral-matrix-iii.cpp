157f20d2bb44318071960d653032f63422773a8e
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-08-10 01:00:38 +0530

    Time: 14 ms (22.81%), Space: 13.8 MB (25.56%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.cpp
@@ -1,16 +1,54 @@
 class Solution {
 public:
-        vector<vector<int>> spiralMatrixIII(int R, int C, int r, int c) {
-        vector<vector<int>> res = {{r, c}};
-        int dx = 0, dy = 1, tmp;
-        for (int n = 0; res.size() < R * C; n++) {
-            for (int i = 0; i < n / 2 + 1; i++) {
-                r += dx, c += dy;
-                if (0 <= r && r < R && 0 <= c && c < C)
-                    res.push_back({r, c});
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> res;
+        int n=rows; int m=cols;
+        int count=0;
+        int total=rows*cols;
+
+        int i=rStart; int j=cStart;
+        int diff=1;
+        while(count<total){
+            //right
+            for(int k=0;k<diff;k++){
+                if(i>=0 && j>=0 && i<n && j<m){
+                    res.push_back({i,j});
+                    count++;
+                }
+                j++;
             }
-            tmp = dx, dx = dy, dy = -tmp;
+            //down
+            for(int k=0;k<diff;k++){
+                if(i>=0 && j>=0 && i<n && j<m){
+                    res.push_back({i,j});
+                    count++;
+                }
+                i++;
+            }
+            if(count>=total) break;
+            diff++; //inc the jump
+
+            //left
+            for(int k=0;k<diff;k++){
+                if(i>=0 && j>=0 && i<n && j<m){
+                    res.push_back({i,j});
+                    count++;
+                }
+                j--;
+            }
+            //up
+            for(int k=0;k<diff;k++){
+                if(i>=0 && j>=0 && i<n && j<m){
+                    res.push_back({i,j});
+                    count++;
+                }
+                i--;
+            }
+
+            diff++; //inc the jump
+
         }
+
         return res;
     }
 };
