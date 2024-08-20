ac33446c2e8f9f5e34701f0bd68c5ed09dcb08fc
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-08-20 12:53:42 +0530

    Time: 0 ms (100%), Space: 7.5 MB (72.78%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int minSteps(int n) {
-        if (n == 1) return 0;
-        
-        int steps = 0;
-        int factor = 2;
-        
-        while (n > 1) {
-            while (n % factor == 0) {
-                steps += factor;
-                n /= factor;
-            }
-            factor++;
+    bool isPrime(int n) {
+        if (n <= 1)
+            return false;
+        if (n == 2 || n == 3)
+            return true;
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
         }
+
+        return true;
+    }
+
+    int minSteps(int n) {
+        if (n == 1)
+            return 0;
+        else if(isPrime(n)) return n;
+
+        for(int i=n/2;i>=1;i--){
+            if(n%i==0 && isPrime(i)){
+                return i+minSteps(n/i);
+            }
+        }
+
+        return n;
         
-        return steps;
     }
 };
