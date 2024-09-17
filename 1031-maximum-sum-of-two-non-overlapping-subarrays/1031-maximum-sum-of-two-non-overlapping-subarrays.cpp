73f0bd28b4566ff0b2fe042c615b12d723de8519
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-18 02:20:42 +0530

    Time: 3 ms (80.25%), Space: 12.6 MB (7.12%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -11,33 +11,38 @@ public:
         return sum;
         
     }
+    int a; int b;
+    int dp[1002][2][2];
     int solve(int ind,vector<int>& nums, int firstLen, int secondLen){
         if(ind>=nums.size()) return 0;
         if(firstLen==0 && secondLen==0) return 0;
+
+        if(dp[ind][firstLen][secondLen]!=-1) return dp[ind][firstLen][secondLen];
         //pick -> two options
         int pick1=0;
         if(firstLen!=0){
-            pick1=sumOfK(ind,nums,firstLen)+solve(ind+firstLen,nums,0,secondLen);
+            pick1=sumOfK(ind,nums,a)+solve(ind+a,nums,0,secondLen);
         }
         int pick2=0;
         if(secondLen!=0){
-            pick2=sumOfK(ind,nums,secondLen)+solve(ind+secondLen,nums,firstLen,0);
+            pick2=sumOfK(ind,nums,b)+solve(ind+b,nums,firstLen,0);
         }
 
         //skip
         int skip=solve(ind+1,nums,firstLen,secondLen);
 
 
-        return max({pick1,pick2,skip});
+        return dp[ind][firstLen][secondLen]=max({pick1,pick2,skip});
     }
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-        
+        a=firstLen; b=secondLen;
+        memset(dp,-1,sizeof(dp));
         prefix.resize(nums.size(),0);
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             prefix[i]=sum;
         }
-        return solve(0,nums,firstLen,secondLen);
+        return solve(0,nums,1,1);
     }
 };
