6e8fa6710d2b5024f36c68717cebe6270fbfe563
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-18 02:20:50 +0530

    Time: 8 ms (32.92%), Space: 12.4 MB (7.83%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -11,38 +11,41 @@ public:
         return sum;
         
     }
-    int a; int b;
+    int firstLen; int secondLen;
     int dp[1002][2][2];
-    int solve(int ind,vector<int>& nums, int firstLen, int secondLen){
+    int solve(int ind,vector<int>& nums, int firstLenDone, int secondLenDone){
         if(ind>=nums.size()) return 0;
-        if(firstLen==0 && secondLen==0) return 0;
+        if(firstLenDone==0 && secondLenDone==0) return 0;
 
-        if(dp[ind][firstLen][secondLen]!=-1) return dp[ind][firstLen][secondLen];
+        if(dp[ind][firstLenDone][secondLenDone]!=-1) return dp[ind][firstLenDone][secondLenDone];
         //pick -> two options
         int pick1=0;
-        if(firstLen!=0){
-            pick1=sumOfK(ind,nums,a)+solve(ind+a,nums,0,secondLen);
+        if(firstLenDone!=0){
+            pick1=sumOfK(ind,nums,firstLen)+solve(ind+firstLen,nums,0,secondLenDone);
         }
         int pick2=0;
-        if(secondLen!=0){
-            pick2=sumOfK(ind,nums,b)+solve(ind+b,nums,firstLen,0);
+        if(secondLenDone!=0){
+            pick2=sumOfK(ind,nums,secondLen)+solve(ind+secondLen,nums,firstLenDone,0);
         }
 
         //skip
-        int skip=solve(ind+1,nums,firstLen,secondLen);
+        int skip=solve(ind+1,nums,firstLenDone,secondLenDone);
 
 
-        return dp[ind][firstLen][secondLen]=max({pick1,pick2,skip});
+        return dp[ind][firstLenDone][secondLenDone]=max({pick1,pick2,skip});
     }
     int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
-        a=firstLen; b=secondLen;
+        this->firstLen = firstLen;  // Assign to class member
+        this->secondLen = secondLen;  // Assign to class member
         memset(dp,-1,sizeof(dp));
+        
         prefix.resize(nums.size(),0);
         int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             prefix[i]=sum;
         }
+        //dp
         return solve(0,nums,1,1);
     }
 };
