2f6f5581ce4c67429959740d3736bff4eb1cc31a
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-19 14:59:08 +0530

    Time: 12 ms (90.54%), Space: 12.4 MB (64.56%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<pair<char, int>> st;
-
-        for (int i = 0; i < s.length(); i++) {
-            // check if last char is same
-            if (!st.empty()) {
-                if (st.top().first == s[i]) {
-                    // if last count==k-1 then pop
-                    if (st.top().second == k - 1) {
-                        while (!st.empty() && st.top().first == s[i])
-                            st.pop();
-                        continue;
-                    } else {
-                        // add one and push
-                        int prevNumber = st.top().second;
-                        st.push({s[i], prevNumber + 1});
-                    }
-                } else {
-                    // first char
-                    st.push({s[i], 1});
-                }
-            } else {
-                // first char
-                st.push({s[i], 1});
+        vector<int> count(s.size(),0);
+        int i = 0,j=0;
+        while(j<s.size())
+        {
+            s[i] = s[j];
+            count[i] = 1;
+            if(i>0 and s[i]==s[i-1])
+            {
+                count[i] += count[i-1];
             }
+            if(count[i]==k)
+            {
+                i -= k;
+            }
+            i++,j++;
         }
-
-        string res = "";
-        while (!st.empty()) {
-            res += st.top().first;
-            st.pop();
-        }
-
-        reverse(res.begin(), res.end());
-
-        return res;
+        string ans = s.substr(0,i);
+        return ans;
     }
 };
