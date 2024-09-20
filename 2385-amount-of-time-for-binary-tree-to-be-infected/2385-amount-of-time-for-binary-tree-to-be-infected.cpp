1c93f48526dff714eb7eae635afd75237959c1a5
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-20 20:35:57 +0530

    Time: 114 ms (96.63%), Space: 92.9 MB (96.92%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,55 +1,43 @@
-    
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+private:
+    int longestPath=0;
+    int solve(TreeNode* root, int start){
+        if(root==NULL) return 0;
+
+        //get both left and right
+        int LH=solve(root->left,start);
+        int RH=solve(root->right,start);
+
+        //special case when root is affected
+        if(root->val==start) {
+            longestPath=max(LH,RH);
+            return -1;
+        }
+        else if(LH<0 or RH<0){
+            //children is start node(infection)
+            longestPath=max(longestPath, abs(LH)+abs(RH));
+            return min(LH,RH)-1;
+        }
+        else{
+            //child is not affected(start node is not the child)
+            return max(LH,RH)+1;
+        }
+
+    }
 public:
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<int, unordered_set<int>> map;
-        convert(root, 0, map);
-        queue<int> q;
-        q.push(start);
-        int minute = 0;
-        unordered_set<int> visited;
-        visited.insert(start);
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-            while (levelSize > 0) {
-                int current = q.front();
-                q.pop();
-
-                for (int num : map[current]) {
-                    if (visited.find(num) == visited.end()) {
-                        visited.insert(num);
-                        q.push(num);
-                    }
-                }
-                levelSize--;
-            }
-            minute++;
-        }
-        return minute - 1;
-    }
-
-    void convert(TreeNode* current, int parent, unordered_map<int, unordered_set<int>>& map) {
-        if (current == nullptr) {
-            return;
-        } 
-        if (map.find(current->val) == map.end()) {
-            map[current->val] = unordered_set<int>();
-        }
-        unordered_set<int>& adjacentList = map[current->val];
-        if (parent != 0) {
-            adjacentList.insert(parent);
-        } 
-        if (current->left != nullptr) {
-            adjacentList.insert(current->left->val);
-        } 
-        if (current->right != nullptr) {
-            adjacentList.insert(current->right->val);
-        }
-        convert(current->left, current->val, map);
-        convert(current->right, current->val, map);
+        solve(root,start);
+        return longestPath;
     }
 };
-
-
