0dfc910a1ccc1434cad46c3c61a137d9915de970
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-20 17:20:34 +0530

    Time: 1616 ms (9.3%), Space: 427.1 MB (5.03%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,46 +1,55 @@
+    
+
 class Solution {
-private:
-    void buildAdj(TreeNode* &root, unordered_map<int, vector<int>>& adj) {
-        if (root == NULL)
-            return;
-
-        // curr node
-        int curr = root->val;
-        if (root->left != NULL) {
-            adj[curr].push_back(root->left->val);
-            adj[root->left->val].push_back(curr);
-        }
-        if (root->right != NULL) {
-            adj[curr].push_back(root->right->val);
-            adj[root->right->val].push_back(curr);
-        }
-        // traverse the tree
-        buildAdj(root->left, adj);
-        buildAdj(root->right, adj);
-    };
-
-    int maximumHeight(int parent,int node, unordered_map<int, vector<int>>& adj) {
-        
-        int height=1;
-
-        for(auto it:adj[node]){
-            if(it!=parent){
-                height=max(height,1+maximumHeight(node,it,adj));
-            }
-        }
-
-        return height;
-        
-    };
-
 public:
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<int, vector<int>> adj;
+        unordered_map<int, unordered_set<int>> map;
+        convert(root, 0, map);
+        queue<int> q;
+        q.push(start);
+        int minute = 0;
+        unordered_set<int> visited;
+        visited.insert(start);
 
-        // build the adj list
-        buildAdj(root, adj);
+        while (!q.empty()) {
+            int levelSize = q.size();
+            while (levelSize > 0) {
+                int current = q.front();
+                q.pop();
 
-        // perform BFS and find out number of levels
-        return maximumHeight(-1,start, adj)-1;
+                for (int num : map[current]) {
+                    if (visited.find(num) == visited.end()) {
+                        visited.insert(num);
+                        q.push(num);
+                    }
+                }
+                levelSize--;
+            }
+            minute++;
+        }
+        return minute - 1;
+    }
+
+    void convert(TreeNode* current, int parent, unordered_map<int, unordered_set<int>>& map) {
+        if (current == nullptr) {
+            return;
+        } 
+        if (map.find(current->val) == map.end()) {
+            map[current->val] = unordered_set<int>();
+        }
+        unordered_set<int>& adjacentList = map[current->val];
+        if (parent != 0) {
+            adjacentList.insert(parent);
+        } 
+        if (current->left != nullptr) {
+            adjacentList.insert(current->left->val);
+        } 
+        if (current->right != nullptr) {
+            adjacentList.insert(current->right->val);
+        }
+        convert(current->left, current->val, map);
+        convert(current->right, current->val, map);
     }
 };
+
+
