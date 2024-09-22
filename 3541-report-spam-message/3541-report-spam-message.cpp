8a755d69824b78989c0344442f2a793307a6b193
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-22 15:42:42 +0530

    Time: 472 ms (6.67%), Space: 288.6 MB (6.67%) - LeetHub

--- 3541-report-spam-message/3541-report-spam-message.cpp
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
         unordered_set<string> st(bannedWords.begin(), bannedWords.end());
-        // for(string &s: bannedWords) st.insert(s);
+        
 
 
         int count=0;
         for(string &s:message){
-            if(st.find(s)!=st.end()) count++;
+            if(st.count(s)>0) count++;
             if(count==2) return true;
         }
 
