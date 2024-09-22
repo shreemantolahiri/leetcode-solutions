ac277ed4915323d0c0b7fbcf18a4e086d8fd8fb9
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-22 18:48:18 +0530

    Time: 6 ms (28.7%), Space: 12.7 MB (28.29%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int dp[366];
-    int nextIndex(int day, vector<int>& days){
-        int ind=lower_bound(days.begin(),days.end(),day)-days.begin();
-        cout<<day<<" index= "<<ind<<endl;
-        return ind;
-    }
-    int solve(int ind, vector<int>& days, vector<int>& costs){
-        if(ind>=days.size()) return 0;
-
-        //check dp
+    int solve(int ind,vector<int>& days, vector<int>& costs){
+        if(ind==days.size()){
+            return 0;
+        }
         if(dp[ind]!=-1) return dp[ind];
-        //we are at day(ind)
+        //we have three options
         //buy 1 day pass
-        int oneDay=costs[0]+solve(ind+1,days, costs);
+        //we move to next index
+        int oneDayPass=costs[0]+solve(ind+1,days,costs);
+        
+        //we can buy 7 day pass
+        //we need to see how many index to skip
+        int valid=days[ind]+6;
+        int i=ind;
+        while(i<days.size() && days[i]<=valid) i++;
 
-        //7 day pass
-        //how much should indexJump by
-        int indexJumpsBy=nextIndex(days[ind]+7,days);
-        int sevenDay=costs[1]+solve(indexJumpsBy,days,costs);
-        //one monthpass
-        indexJumpsBy=nextIndex(days[ind]+30,days);
-        int thirtyDay=costs[2]+solve(indexJumpsBy,days,costs);
+        //i is at first invalid day
+        int sevenDayPass=costs[1]+solve(i,days,costs);
 
-        return dp[ind]=min({oneDay,sevenDay,thirtyDay});
+        //we buy 30 day pass
+        valid=days[ind]+29;
+        i=ind;
+        while(i<days.size() && days[i]<=valid) i++;
+        int thirtyDayPass=costs[2]+solve(i,days,costs);
+
+        return dp[ind]=min({oneDayPass,sevenDayPass,thirtyDayPass});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // int n=days.size();
         memset(dp,-1,sizeof(dp));
         return solve(0,days,costs);
     }
