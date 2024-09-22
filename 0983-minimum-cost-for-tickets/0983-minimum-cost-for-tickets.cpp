5851c32a38ce25ef8472a8f335f3c1c254130029
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-09-22 18:46:30 +0530

    Time: 7 ms (14.86%), Space: 12.8 MB (25.89%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    int dp[366][1600];
-    int solve(int ind,int valid,vector<int>& days, vector<int>& costs){
-        if(ind==days.size()){
-            return 0;
-        }
-        if(dp[ind][valid]!=-1) return dp[ind][valid];
-        //if day is valid just continue
-        if(valid>=days[ind]) 
-        return dp[ind][valid]=solve(ind+1,valid,days,costs);
+    int dp[366];
+    int nextIndex(int day, vector<int>& days){
+        int ind=lower_bound(days.begin(),days.end(),day)-days.begin();
+        cout<<day<<" index= "<<ind<<endl;
+        return ind;
+    }
+    int solve(int ind, vector<int>& days, vector<int>& costs){
+        if(ind>=days.size()) return 0;
 
-        else{
-            //have to buy
-            return dp[ind][valid]=min({
-                solve(ind+1,days[ind],days,costs)+costs[0],
-                solve(ind+1,days[ind]+6,days,costs)+costs[1],
-                solve(ind+1,days[ind]+29,days,costs)+costs[2]
+        //check dp
+        if(dp[ind]!=-1) return dp[ind];
+        //we are at day(ind)
+        //buy 1 day pass
+        int oneDay=costs[0]+solve(ind+1,days, costs);
 
-            });
-        }
+        //7 day pass
+        //how much should indexJump by
+        int indexJumpsBy=nextIndex(days[ind]+7,days);
+        int sevenDay=costs[1]+solve(indexJumpsBy,days,costs);
+        //one monthpass
+        indexJumpsBy=nextIndex(days[ind]+30,days);
+        int thirtyDay=costs[2]+solve(indexJumpsBy,days,costs);
+
+        return dp[ind]=min({oneDay,sevenDay,thirtyDay});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         memset(dp,-1,sizeof(dp));
-        return solve(0,0,days,costs);
+        return solve(0,days,costs);
     }
 };
