879b24a38903ca303a1786b45534890fb94e3843
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-10-06 02:25:32 +0530

    Time: 9 ms (48.35%), Space: 8.6 MB (92.5%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int n = s2.length();
-        int m = s1.length();
-        unordered_map<char, int> mp;
-        for (char ch : s1)
-            mp[ch]++;
-
-        unordered_map<char, int> mp1;
-
-        int i = 0, j = 0;
-
-        while (j < n) {
-            // if (j - i == m)
-            //     return true;
-            char ch = s2[j];
-            if (mp[ch] == 0) {
-                // does not exist
-                mp1.clear();
-                i = j + 1;
-                j++;
-            } else if (mp1[ch] < mp[ch]) {
-                mp1[ch]++;
-                j++;
-            } else {
-                mp1[ch]++;
-                while (i < j && mp1[ch] > mp[ch]) {
-                    mp1[s2[i]]--;
-                    i++;
-                }
-                j++;
-            }
-
-            if (j - i == m)
-                return true;
+        if (s1.length() > s2.length()) return false;
+        
+        vector<int> s1_count(26, 0);
+        vector<int> window_count(26, 0);
+        
+        // Count the frequencies of characters in s1
+        for (char c : s1) {
+            s1_count[c - 'a']++;
         }
-
+        
+        // Initialize the window
+        for (int i = 0; i < s1.length(); i++) {
+            window_count[s2[i] - 'a']++;
+        }
+        
+        if (s1_count == window_count) return true;
+        
+        // Slide the window
+        for (int i = s1.length(); i < s2.length(); i++) {
+            window_count[s2[i - s1.length()] - 'a']--;
+            window_count[s2[i] - 'a']++;
+            
+            if (s1_count == window_count) return true;
+        }
+        
         return false;
     }
 };
