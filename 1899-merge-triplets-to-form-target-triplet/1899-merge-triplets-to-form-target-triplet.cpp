de971d258ccddf532f8c25675eca5e9e738b5bea
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-12-02 17:01:13 +0530

    Time: 0 ms (100%), Space: 150.5 MB (40.93%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -2,33 +2,28 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         bool found1 = false, found2 = false, found3 = false;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < triplets.size(); j++) {
-                if (i == 0 && triplets[j][i] == target[i]) {
-                    if (triplets[j][1] <= target[1] &&
-                        triplets[j][2] <= target[2]) {
-                        found1 = true;
-                        break;
-                    }
+
+        for (int j = 0; j < triplets.size(); j++) {
+            if (triplets[j][0] == target[0]) {
+                if (triplets[j][1] <= target[1] &&
+                    triplets[j][2] <= target[2]) {
+                    found1 = true;
                 }
-                if (i == 1 && triplets[j][i] == target[i]) {
-                    if (triplets[j][0] <= target[0] &&
-                        triplets[j][2] <= target[2]) {
-                        found2 = true;
-                        break;
-                    }
+            }
+            if (triplets[j][1] == target[1]) {
+                if (triplets[j][0] <= target[0] &&
+                    triplets[j][2] <= target[2]) {
+                    found2 = true;
                 }
-                else if (i == 2 && triplets[j][i] == target[i]) {
-                    if (triplets[j][1] <= target[1] &&
-                        triplets[j][0] <= target[0]) {
-                        found3 = true;
-                        break;
-                    }
+            }
+            if (triplets[j][2] == target[2]) {
+                if (triplets[j][1] <= target[1] &&
+                    triplets[j][0] <= target[0]) {
+                    found3 = true;
                 }
             }
         }
-        return (found1 && found2 && found3);
 
-        
+        return (found1 && found2 && found3);
     }
 };
