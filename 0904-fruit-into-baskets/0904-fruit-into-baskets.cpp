5e40bd9015f67c39e6108578997425a72c44f3c2
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-12-12 00:43:11 +0530

    Time: 52 ms (54.39%), Space: 75.1 MB (66.29%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int types=0;
-        int maxLen=1;
+        //fruit, count
         unordered_map<int,int> mp;
-        int l=0;
-        for(int r=0;r<fruits.size();r++){
-            if(mp[fruits[r]]==0){
-                //new type
-                if(types<2){
-                    mp[fruits[r]]++;
-                    types++;
-                    maxLen=max(maxLen,r-l+1);
-                }
-                else{
-                    //until becomes zero
-                    while(l<=r && types==2){
-                        mp[fruits[l]]--;
-                        if(mp[fruits[l]]==0){
-                            types--;
-                        }
-                        l++;
-                    }
-                    mp[fruits[r]]++;
-                    types++;
-                }
-            }
+        int maxLen=0;
+        int l=0,r=0;
+        int n=fruits.size();
+        for(;r<n;r++){
+            mp[fruits[r]]++;
+
+            if(mp.size()<=2) maxLen=max(maxLen,r-l+1);
             else{
-                //old fruit
-                mp[fruits[r]]++;
-                    
-                maxLen=max(maxLen,r-l+1);
+                mp[fruits[l]]--;
+                if(mp[fruits[l]]==0) mp.erase(fruits[l]);
+                l++;
             }
         }
 
         return maxLen;
-
     }
 };
