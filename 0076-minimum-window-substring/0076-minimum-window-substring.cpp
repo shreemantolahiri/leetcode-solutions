9dc50755a9693a9c7f8c8e8f2f8024f8a799cb86
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2024-12-19 03:21:21 +0530

    Time: 27 ms (23.5%), Space: 11.2 MB (43.09%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int getIndex(char c){
+        if(c>='a' && c<='z') return c-'a';
+
+        else return 26+c-'A';
+    }
+    bool cmp(vector<int> &smap,vector<int> &tmap){
+        for(int i=0;i<52;i++){
+            if(tmap[i]>smap[i]) return false;
+        }
+
+        return true;
+    }
+    string minWindow(string s, string t) {
+        vector<int> tmap(52,0), smap(52,0);
+
+        // for(auto c:s) smap[c-'A']++;
+        for(char c:t) {
+            tmap[getIndex(c)]++;
+        }
+
+        int left=0,right=0;
+        int minLen=INT_MAX;
+        int minInd=0;
+
+        for(;right<s.length();right++){
+            smap[getIndex(s[right])]++;
+            // cout<<minLen<<endl;
+            // cout<<left<<" "<<right<<endl;
+
+            // for(int i=0;i<52;i++){
+            //     cout<<smap[i]<<" ";
+            // }
+            // cout<<endl;
+            while(cmp(smap,tmap)){
+                if(right-left+1<minLen){
+                    minInd=left;
+                    minLen=right-left+1;
+                }
+                //remove
+                smap[getIndex(s[left])]--;
+                left++;
+            }
+        }
+        if(minLen==INT_MAX) return "";
+        string res=s.substr(minInd,minLen);
+
+        return res;
+    }
+};
