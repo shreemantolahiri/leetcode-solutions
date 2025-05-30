f8ccda5fe9b3b80c05a2b50e38b6216503b49b56
Shreemanto Lahiri <shreemantolahiri@gmail.com>
2025-05-31 02:47:33 +0530

    Time: 18 ms (48.43%), Space: 62.1 MB (58.54%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,50 +1,34 @@
 class Solution {
+    void BFS(int[] edges, int dist[],int start){
+        Queue<Integer> q= new LinkedList<>();
+
+        q.offer(start);
+        while(!q.isEmpty()){
+            int node=q.poll();
+            int distance=dist[node];
+            int destNode=edges[node];
+            
+            if(destNode==-1) continue; //end of graph
+            else{
+                if(dist[destNode]<=distance+1) continue;
+                else{
+                    dist[destNode]=distance+1;
+                    q.offer(destNode);
+                }
+            }
+
+        }
+    }
     public int closestMeetingNode(int[] edges, int node1, int node2) {
 
         int n = edges.length;
         int[] n1_dist = new int[n], n2_dist = new int[n];
-        for (int i = 0; i < n; i++) {
-            n1_dist[i] = Integer.MAX_VALUE;
-            n2_dist[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(n1_dist,Integer.MAX_VALUE);
+        Arrays.fill(n2_dist,Integer.MAX_VALUE);
         n1_dist[node1]=0; n2_dist[node2]=0;
-        Queue<Integer> q= new LinkedList<>();
 
-        //for node 1
-        q.offer(node1);
-        while(!q.isEmpty()){
-            int node=q.poll();
-            int distance=n1_dist[node];
-            int destNode=edges[node];
-            
-            if(destNode==-1) continue; //end of graph
-            else{
-                if(n1_dist[destNode]<=distance+1) continue;
-                else{
-                    n1_dist[destNode]=distance+1;
-                    q.offer(destNode);
-                }
-            }
-
-        }
-
-        //for second node
-        q.offer(node2);
-        while(!q.isEmpty()){
-            int node=q.poll();
-            int distance=n2_dist[node];
-            int destNode=edges[node];
-            
-            if(destNode==-1) continue; //end of graph
-            else{
-                if(n2_dist[destNode]<=distance+1) continue;
-                else{
-                    n2_dist[destNode]=distance+1;
-                    q.offer(destNode);
-                }
-            }
-        }
-
+        BFS(edges,n1_dist,node1);
+        BFS(edges,n2_dist,node2);
 
         //find min distance
         int minDist=Integer.MAX_VALUE;
